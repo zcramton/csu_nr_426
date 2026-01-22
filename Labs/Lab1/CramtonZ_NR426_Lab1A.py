2425f2f7e81a011099f9d8e91aac481617c8d66f
ZCramton <zachary@cramton.com>
2026-01-22 16:27:25 -0700

    Initial Commit

--- Labs/Lab1/CramtonZ_NR426_Lab1A.py
+++ Labs/Lab1/CramtonZ_NR426_Lab1A.py
@@ -19,7 +19,17 @@ Date: 22JAN2025
 
 # All import statements for modules go here:
 
+"""
+To import packages use:
+Import packageName
 
+Or
+
+From packageName Import * (for all) or functionX (for a specific func)
+
+For example: from math import *
+"""
+from os import *
 
 #### In-class activities (DEMO)  ####
 
@@ -39,26 +49,58 @@ yearCurrent = 2026
 
 yearsResident = yearCurrent - yearMoved
 
-print (f"{studentName} is {age} years old and has lived in {city} for {yearsResident} years since {yearMoved}.")
+print (f"{studentName} is {age} years old and has lived in {city} for "
+       f"{yearsResident} years since {yearMoved}.")
 
+# Exploring len()
+
+print (type(age))
+print (type(studentName))
+print (len(studentName))
+
+# Indexing
+
+"""
+For indexing, try things out in the console before inserting into the code, 
+in general the format is:
+
+var[x:y]
+
+With indexing from the left starting at 0 and indexing from the right starting at -1.
+"""
 #### Lab 1A Self-directed activities, done on your own  ####
 
 ##  3. Create two number variables, add them and print their sum
+number1 = 5
+number2 = 2
 
-
+print (f"The sum is {number1 + number2}.")
 
 ##  4. Create a variable for your favorite elevation in feet (Fort Collins is 4980). Write the code to convert
 #    that value into meters (just multiply by a conversion factor)
 #    Create and print a well-formatted statement like 5280 feet is equal to 1604 meters
 
+# Elevation of the Colorado low point on the Arikaree River
 
+# Input elev
+elevFT = 3317
 
+# Unit Conversion (1 ft = 0.3048 m)
+elevM = elevFT * 0.3048
+
+# Print rounded value in an answer statement
+print (f"The elevation of the Colorado low point on the Arikaree River is "
+       f"{round(elevM)} meters or {elevFT} feet.")
 
 ##  5. Create a string variable and print with your own text to make a complete sentence
 #    example: Create a variable for "Park_boundary.shp" ; sample result: The file name is Park_boundary.shp
 
+# File name(s)
+file1 = "Park_boundary.shp"
 
-
+# Print file name
+printFile = file1
+print(f"The file name is {printFile}.")
 
 ##  6. Concatenate a number and a string variable using both str() and the f-string method.
 #     Create a string and a number variable, then write the code to return a nicely formatted
@@ -66,21 +108,23 @@ print (f"{studentName} is {age} years old and has lived in {city} for {yearsResi
 #     Add your own additional text as needed
 #     (ex: Programming for GIS 1 is 8 weeks long)
 
+# Course details
+courseName = "Programming for GIS I"
+courseDurationWk = 8
 
-
-
+print (f"{courseName} is {courseDurationWk} weeks long.")
 
 ##  7. Work with index notation for a string (and recall that -1 indexes from the end)
 #     a- Create a variable to represent a shapefile (just make something up): C:\student\NR426\LandCover.shp
-
+path = r"C:\Users\Zacha\College Classes\NR426\CO_NDVI.shp"
 
 #     b- Write the code using index notation to parse out and print just the shapefile name without any path or extension
 #     ex: The dataset name is: LandCover
-
+print (f"The dataset name is {path[-11:-4]}.")
 
 #    c- Write the code using index notation to parse out and print just the file extension
 #     ex: The file extension is .shp
-
+print (f"The file extention is {path[-4:]}.")
 #    d-	Can you find another approach to splitting the file name from the path? (Hint: look in the os module)
 
 
